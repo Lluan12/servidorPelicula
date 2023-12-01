32881eaa084703df2799ace0705f953de6ddd418
Lluan12 <103333263+Lluan12@users.noreply.github.com>
2023-11-30 23:28:10 -0700

    Terminacion api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const peliculasRouter = require('./routers/peliculas.router');
+const usersRouter = require("./routers/users.router");
 require('./utils/mongoConnection');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(morgan('dev'));
 
 app.use(express.json({limit: '50mb'}));
 app.use('/peliculas', peliculasRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, (req, res) => {
 	console.log("Escuchando en el puerto 3000");
--- routers/peliculas.router.js
+++ routers/peliculas.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const peliculas = require('../controllers/peliculas.controller');
+const authMiddleware = require('../utils/auth.middleware');
 
 router.get('/', peliculas.getPeliculas);
 router.get('/:peliculaId', peliculas.getPeliculasById);
-router.post('/', peliculas.createPelicula);
-router.put('/:peliculaId', peliculas.updatePelicula);
-router.delete('/:peliculaId', peliculas.deletePelicula);
+router.post('/', authMiddleware.authenticateToken, peliculas.createPelicula);
+router.put('/:peliculaId', authMiddleware.authenticateToken, peliculas.updatePelicula);
+router.delete('/:peliculaId', authMiddleware.authenticateToken, peliculas.deletePelicula);
 
 module.exports = router;
